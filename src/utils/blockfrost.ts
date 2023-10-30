1d4b35b2165a25b48f43a574d042330d233a579b
Nguyễn Khánh <nguyenkhanh17112003@gmail.com>
2023-10-31 01:33:48 +0700

    feat: from address query stakekey

--- src/utils/blockfrost.ts
+++ src/utils/blockfrost.ts
@@ -3,7 +3,7 @@ import { BlockFrostAPI } from "@blockfrost/blockfrost-js";
 dotenv.config();
 
 const apiBlockfrost = new BlockFrostAPI({
-    projectId: process.env.BLOCKFROST_PROJECT_ID_SECRET_PREPROD!,
+    projectId: "preprodaXBLgbqqCAo5wMCdB77sUusgmx2RcgtH",
 });
 
 export default apiBlockfrost;
