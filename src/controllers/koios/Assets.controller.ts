f5e85e72afebbd8989f33ce003aff58b0e5c9f83
Nguyễn Khánh <nguyenkhanh17112003@gmail.com>
2023-11-07 04:20:47 +0700

    testcase: get current address holding policyId and assetName

--- src/controllers/koios/Assets.controller.ts
+++ src/controllers/koios/Assets.controller.ts
@@ -1,19 +1,26 @@
 import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 import { get, post } from "../../utils/koios";
-import stringToHex from "../../helpers/convertHex";
+import { NotFound } from "../../errors";
 
 class AssetsController {
+    /**
+     * @description Get the current address holding the asset
+     * @param request body: { policyId, assetName } required
+     * @param response current address
+     */
     async assetNftAddress(request: Request, response: Response) {
         try {
             const { policyId, assetName } = request.body;
+            if (!policyId && !assetName) {
+                return response
+                    .status(StatusCodes.NOT_FOUND)
+                    .json(new NotFound("policyId and assetName has been required."));
+            }
 
-            const data = await get(
-                `/asset_nft_address?_asset_policy=${policyId}&_asset_name=${assetName}`,
-                {},
-            );
+            const data = await get(`/asset_nft_address?_asset_policy=${policyId}&_asset_name=${assetName}`, {});
 
-            response.status(StatusCodes.OK).json(data);
+            response.status(StatusCodes.OK).json({ address: data[0].payment_address });
         } catch (error) {
             response.status(StatusCodes.BAD_REQUEST).json({
                 message: error,
@@ -21,14 +28,22 @@ class AssetsController {
         }
     }
 
+    /**
+     *
+     * @param request body: { policyId, assetName } required
+     * @param response
+     */
+
     async assetInfomation(request: Request, response: Response) {
         try {
             const { policyId, assetName } = request.body;
+            if (!policyId && !assetName) {
+                return response
+                    .status(StatusCodes.NOT_FOUND)
+                    .json(new NotFound("policyId and assetName has been required."));
+            }
 
-            const data = await get(
-                `/asset_info?_asset_policy=${policyId}&_asset_name=${assetName}`,
-                {},
-            );
+            const data = await get(`/asset_info?_asset_policy=${policyId}&_asset_name=${assetName}`, {});
 
             response.status(StatusCodes.OK).json(data);
         } catch (error) {
@@ -70,10 +85,7 @@ class AssetsController {
         try {
             const { policyId, assetName } = request.body;
 
-            const data = await get(
-                `/asset_summary?_asset_policy=${policyId}&_asset_name=${assetName}`,
-                {},
-            );
+            const data = await get(`/asset_summary?_asset_policy=${policyId}&_asset_name=${assetName}`, {});
 
             response.status(StatusCodes.OK).json(data);
         } catch (error) {
