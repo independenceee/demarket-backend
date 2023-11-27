111fff4f0462e9554ee92d05db800520eeac8d2d
Nguyễn Khánh <nguyenkhanh17112003@gmail.com>
2023-11-27 19:31:30 +0700

    fix: get nft from cart using address wallet

--- src/services/demarket/Guide.service.ts
+++ src/services/demarket/Guide.service.ts
@@ -4,20 +4,13 @@ import prisma, { Guide } from "../../models";
 class GuideService {
     async findGuideById(id: string): Promise<Guide | null> {
         try {
-            const guide = await prisma.guide.findFirst({
-                where: {
-                    id: id,
-                },
-            });
-
-            if (guide) {
-                return guide;
-            }
+            const guide: Guide | null = await prisma.guide.findFirst({ where: { id: id } });
+            return guide;
         } catch (error) {
             throw new ApiError(error);
+        } finally {
+            await prisma.$disconnect();
         }
-
-        return null;
     }
 }
 
